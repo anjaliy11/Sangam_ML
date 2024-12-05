2ded5e89481fe6046d6ae72b482edb0681db2313
anjaliy11 <anjaliyadavknp9450@gmail.com>
2024-12-05 23:16:07 +0530

    update

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,4 @@
-// const express = require('express');
-// const axios = require('axios');
-// const router = express.Router();
+
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
